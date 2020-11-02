289328a3aa6b8993f2e82eace76b569676c0d863
earlwlkr <mike358015@gmail.com>
2020-11-02 09:25:15 +0700

    Persistent spacing

--- components/ForecastList/ForecastList.js
+++ components/ForecastList/ForecastList.js
@@ -4,13 +4,13 @@ import styles from './ForecastList.module.css';
 
 export default function ForecastList({ items }) {
   if (!items.length) {
-    return <div>No items</div>
+    return <div>No items</div>;
   }
   console.log(items);
   return (
     <div className={styles.grid}>
       {items.map((item) => (
-        <ForecastItem data={item} />
+        <ForecastItem key={item.date} data={item} />
       ))}
     </div>
   );
--- components/ForecastView/ForecastView.js
+++ components/ForecastView/ForecastView.js
@@ -4,7 +4,7 @@ import ForecastList from 'components/ForecastList';
 export default function ForecastView({ data }) {
   return (
     <>
-      <h2>{data?.location}</h2>
+      {data?.location && <h2>{data?.location}</h2>}
 
       <ForecastList items={data?.forecast || []} />
     </>
