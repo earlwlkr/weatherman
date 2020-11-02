a073de16460a11be73bf54bddaee3dbe8a1f0215
earlwlkr <mike358015@gmail.com>
2020-11-02 20:36:03 +0700

    Add switch component

--- components/Home/Home.js
+++ components/Home/Home.js
@@ -4,7 +4,9 @@ import debounce from 'lodash/debounce';
 
 import { useTemperatureUnitFunctions } from 'contexts/TemperatureUnitContext';
 import SearchBar from 'components/SearchBar';
+import Switch from 'components/Switch';
 import ForecastView from 'components/ForecastView';
+
 import styles from './Home.module.css';
 
 const fetchForecast = (url, query) =>
@@ -31,15 +33,36 @@ export default function Home({ initQuery }) {
     console.log('fetch forecast error', error);
   }
 
+  const onTempUnitChange = (value) => {
+    switch (value) {
+      case 'F': {
+        switchToFahrenheit();
+        break;
+      }
+      default:
+      case 'C': {
+        switchToCelsius();
+        break;
+      }
+    }
+  };
+
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1 className={styles.title}>Weatherman</h1>
+        <h1 className={styles.title}>
+          <span style={{ color: '#4630eb' }}>Weather</span>man
+        </h1>
 
         <p className={styles.description}>
           <SearchBar onChange={onQueryChange} />
-          <button onClick={switchToCelsius}>°C</button>
-          <button onClick={switchToFahrenheit}>°F</button>
+          <Switch
+            options={[
+              { key: 'C', value: '°C' },
+              { key: 'F', value: '°F' },
+            ]}
+            onChange={onTempUnitChange}
+          />
         </p>
 
         {isLoading ? <div>Loading...</div> : <ForecastView data={data} />}
---
+++ components/Switch/Switch.js
@@ -0,0 +1,33 @@
+import * as React from 'react';
+
+import styles from './Switch.module.css';
+
+export default function Switch({ options, onChange }) {
+  const [selected, setSelected] = React.useState(0);
+
+  const onSwitchChange = (index) => {
+    setSelected(index);
+    const value = options[index]?.key;
+    if (value) {
+      onChange(value);
+    }
+  };
+
+  return (
+    <div className={styles.switchGroup}>
+      {options.map((option, index) => {
+        return (
+          <div
+            key={option.key}
+            className={
+              selected === index ? styles.switchItemSelected : styles.switchItem
+            }
+            onClick={() => onSwitchChange(index)}
+          >
+            {option.value}
+          </div>
+        );
+      })}
+    </div>
+  );
+}
